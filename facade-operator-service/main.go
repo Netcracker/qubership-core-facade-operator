d90ade5b8e5245614af62703c59d8030295889a6
Sergey Lisovoy <lis0x90@gmail.com>
2025-05-14 16:49:20 +0500

    refactor: rest client

--- facade-operator-service/main.go
+++ facade-operator-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/netcracker/qubership-core-facade-operator/facade-operator-service/v2/lib"
+	fiberSec "github.com/netcracker/qubership-core-lib-go-fiber-server-utils/v2/security"
 	"github.com/netcracker/qubership-core-lib-go/v3/security"
 	"github.com/netcracker/qubership-core-lib-go/v3/serviceloader"
 	"github.com/netcracker/qubership-core-lib-go/v3/utils"
@@ -10,6 +11,7 @@ import (
 func init() {
 	serviceloader.Register(1, &security.DummyToken{})
 	serviceloader.Register(1, utils.NewResourceGroupAnnotationsMapper("qubership.cloud"))
+	serviceloader.Register(1, &fiberSec.DummyFiberServerSecurityMiddleware{})
 }
 
 func main() {
